d10781225cb040cc321ea9628b01c14c0652d283
Edu Gabriel <edu.santos@dcomp.ufs.br>
2025-12-16 21:17:08 -0300

    Adição da mecânica de Dash
    Fiz a mecânica de dash, onde o personagem com um simples botão ativa um boost de velocidade instântaneo. Ele sempre é possível a partir do momento que ele toca no chão.
    Podemos definir um cooldown, que ainda está sendo analisado mas acredito que está bom.
    Foi adicionado sprites simples para o dash (que inclusive estão bem feioskk) mas posteriormente será consertado quando o foco for animação.
    A ideia é de que o dash resulte um dano ao inimigo caso entre em contato, porém, como ainda não temos sistemas de combate e de vida, isso ainda não foi implementado.

--- Metroidvania/Assets/PlayerController.cs
+++ Metroidvania/Assets/PlayerController.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector2 moveInput;
     Animator animator;
     private Rigidbody2D rb;
-    private float xAxis;
 
     [Header("Movimento")]
     [SerializeField] private float walkSpeed = 20;
@@ -23,7 +24,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
 
+    [Header("Dash")]
+    // Dash variables can be added here
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] private float dashCooldown = 1f;
+    private bool isDashing;
+    private bool canDash = true;
+
     public static PlayerController instance;
+
+    private float lastXDirection = 1;
 
     private void Awake()
     {
@@ -48,22 +59,25 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if(isDashing) return;
         if (Keyboard.current == null) return;
 
         CheckGround();
         GetInputs();
         Move();
         JumpInput();
+        DashInput();
         Flip();
     }
 
+
     void Flip()
     {
-        if(xAxis < 0)
+        if(moveInput.x < 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        else if(xAxis > 0)
+        else if(moveInput.x > 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
@@ -73,22 +87,32 @@ public class PlayerController : MonoBehaviour
     {
         // Se está no chão E não está subindo muito rápido (ex: acabou de pular)
         // Aumentei a tolerância de 0.1f para 3.0f para evitar o bug de travar
-        if (IsGrounded() && rb.linearVelocity.y < 3f) 
+        if (IsGrounded()) 
         {
-            remainingJumps = maxJumps;
+            if(rb.linearVelocity.y < 3f) remainingJumps = maxJumps;
+            canDash = true;
         }
+        
     }
 
     void GetInputs()
     {
-        xAxis = 0;
-        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) xAxis = -1;
-        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) xAxis = 1;
+        moveInput = Vector2.zero;
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) moveInput.x = -1;
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) moveInput.x = 1;
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) moveInput.y = 1;
+        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) moveInput.y = -1;
+
+        // Atualiza a última direção horizontal para saber para onde dar dash se estiver parado
+        if (moveInput.x != 0)
+        {
+            lastXDirection = moveInput.x;
+        }
     }
 
     void Move()
     {
-        rb.linearVelocity = new Vector2(walkSpeed * xAxis, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(walkSpeed * moveInput.x, rb.linearVelocity.y);
         animator.SetBool("Walking", rb.linearVelocityX != 0 && IsGrounded());
     }
 
@@ -97,16 +121,20 @@ public class PlayerController : MonoBehaviour
         // Pulo
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            if (remainingJumps > 0)
+            if (remainingJumps > 0 && IsGrounded())
             {
                 if(remainingJumps != 1)
                 {
                     rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
                 } else
                 {
-                    rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce*(float) 0.8);
+                    rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce * 0.8f);
                 }
                 remainingJumps--;
+            } else if (remainingJumps > 0 && !IsGrounded())
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce * 0.8f);
+                remainingJumps = 0;
             }
         }
 
@@ -137,5 +165,58 @@ public class PlayerController : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(groundCheckB.position, groundCheckRadius);
         }
+    }
+
+    void DashInput()
+    {
+        if (Keyboard.current.leftShiftKey.wasPressedThisFrame && canDash)
+        {
+            StartCoroutine(PerformDash());
+            animator.SetBool("Dash", true);
+        }
+    }
+
+    private IEnumerator PerformDash()
+    {
+        isDashing = true;
+        canDash = false;
+        
+        // Guarda a gravidade e a escala original
+        float originalGravity = rb.gravityScale;
+        Vector3 originalScale = transform.localScale; 
+
+        rb.gravityScale = 0;
+        rb.linearVelocity = Vector2.zero; 
+
+        // Define direção do Dash
+        Vector2 dashDir;
+        if (moveInput == Vector2.zero) dashDir = new Vector2(lastXDirection, 0);
+        else dashDir = moveInput.normalized;
+        if(dashDir.y != 0)
+        {
+            animator.SetBool("DashUp", true);
+        }
+        rb.linearVelocity = dashDir * dashSpeed;
+        // Animação do dash (escalar o personagem para dar efeito de "achatamento")
+        float timer = 0f;
+        while (timer < dashDuration)
+        {
+            timer += Time.deltaTime;
+            if(dashDir.y != 0)
+                transform.localScale = new Vector3(originalScale.x * 0.45f, originalScale.y, 1);
+            else
+                transform.localScale = new Vector3(originalScale.x * 5f, originalScale.y * 0.45f, 1);
+
+            yield return null; // Espera o próximo frame
+        }
+
+        // Restaura tudo ao final
+        transform.localScale = originalScale; // Garante que voltou ao tamanho exato
+        rb.gravityScale = originalGravity;
+        rb.linearVelocity = Vector2.zero;
+        isDashing = false;
+        animator.SetBool("Dash", false);
+        animator.SetBool("DashUp", false);
+        yield return new WaitForSeconds(dashCooldown);
     }
 }
